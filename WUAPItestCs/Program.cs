62815df30eea003428f73f1ccba88a19642d98df
Christian Ullrich <chris@chrullrich.net>
2020-08-12 22:05:33 +0200

    Fixed reboot logic, better exit codes, automated cleanup.

--- WUAPItestCs/Program.cs
+++ WUAPItestCs/Program.cs
@@ -1,59 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using BetterWin32Errors;
 using WUApiLib;
 
 namespace WUAPItestCs
 {
-    class Program
+    internal class Program
     {
         private const string RebootFlagFileName = "C:\\Windows\\reboot.required";
 
-        static int Main(string[] args)
+        private static int Main(string[] args)
         {
             try {
                 return InstallUpdates();
             } catch (COMException e) {
                 Console.Error.WriteLine(e.Message);
                 Console.Error.WriteLine(e.StackTrace);
-                return (int)BetterWin32Errors.Win32Error.ERROR_INSTALL_FAILURE;
+                return (int)Win32Error.ERROR_INSTALL_FAILURE;
             }
         }
 
-        static int InstallUpdates()
+        private static int InstallUpdates()
         {
-            UpdateSession session = new UpdateSession();
+            var session = new UpdateSession();
             session.ClientApplicationID = "WUAPItestCs";
-            IUpdateSearcher searcher = session.CreateUpdateSearcher();
+            var searcher = session.CreateUpdateSearcher();
 
             Console.WriteLine("Searching for updates ...");
 
-            ISearchResult searchResult = searcher.Search("IsInstalled=0 and Type='Software' and IsHidden=0");
-            UpdateCollection found = searchResult.Updates;
+            var searchResult = searcher.Search("IsInstalled=0 and Type='Software' and IsHidden=0");
+            var found = searchResult.Updates;
 
             if (found.Count == 0) {
                 Console.WriteLine("No applicable updates found.");
-                return 0;
+                return (int)BetterWin32Errors.Win32Error.ERROR_SUCCESS;
             }
 
             Console.WriteLine("List of applicable updates:");
 
-            UpdateCollection selected = new UpdateCollection();
-            foreach (IUpdate update in found)
-            {
+            var selected = new UpdateCollection();
+            foreach (IUpdate update in found) {
                 Console.WriteLine(update.Title);
 
                 if (update.InstallationBehavior.CanRequestUserInput) {
                     Console.WriteLine("    skipped because it requires user input");
                     continue;
-                } else if (!update.EulaAccepted) {
+                }
+
+                if (!update.EulaAccepted) {
                     Console.WriteLine("    skipped because EULA declined");
                     continue;
-                } else if (update.Title.StartsWith("Feature update")) {
+                }
+
+                if (update.Title.StartsWith("Feature update")) {
                     Console.WriteLine("    skipped because feature updates are interactive");
                     continue;
                 }
@@ -63,20 +63,20 @@ namespace WUAPItestCs
 
             if (selected.Count == 0) {
                 Console.WriteLine("All applicable updates are skipped");
-                return 0;
+                return (int)BetterWin32Errors.Win32Error.ERROR_SUCCESS;
             }
 
             Console.WriteLine("Downloading updates ...");
 
-            UpdateDownloader downloader = session.CreateUpdateDownloader();
+            var downloader = session.CreateUpdateDownloader();
             downloader.Updates = selected;
             downloader.Download();
 
-            bool reboot = false;
+            var reboot = false;
 
             Console.WriteLine("Downloaded updates:");
 
-            UpdateCollection downloaded = new UpdateCollection();
+            var downloaded = new UpdateCollection();
 
             foreach (IUpdate update in selected) {
                 if (update.IsDownloaded) {
@@ -84,7 +84,7 @@ namespace WUAPItestCs
                     downloaded.Add(update);
                 }
 
-                reboot |= (update.InstallationBehavior.RebootBehavior != InstallationRebootBehavior.irbNeverReboots);
+                reboot |= update.InstallationBehavior.RebootBehavior != InstallationRebootBehavior.irbNeverReboots;
             }
 
             if (downloaded.Count == 0) {
@@ -92,43 +92,45 @@ namespace WUAPItestCs
                 return 1;
             }
 
-            if (reboot) {
-                Console.WriteLine("These updates may require a reboot.");
-            }
+            if (reboot) Console.WriteLine("These updates may require a reboot.");
 
             Console.WriteLine("Installing updates ...");
 
-            IUpdateInstaller installer = session.CreateUpdateInstaller();
+            var installer = session.CreateUpdateInstaller();
             installer.Updates = downloaded;
 
-            IInstallationResult installationResult = installer.Install();
-            OperationResultCode operationResultCode = installationResult.ResultCode;
-            Console.WriteLine("Installation result: " + operationResultCode.ToString());
+            var installationResult = installer.Install();
+            var operationResultCode = installationResult.ResultCode;
+            Console.WriteLine("Installation result: " + operationResultCode);
 
             if (operationResultCode == OperationResultCode.orcAborted
-                   || operationResultCode == OperationResultCode.orcFailed) {
+                || operationResultCode == OperationResultCode.orcFailed) {
                 Console.Error.WriteLine("Installation aborted or failed.");
-                return 1;
+                return (int)BetterWin32Errors.Win32Error.ERROR_INSTALL_FAILED;
             }
 
             if (installationResult.RebootRequired) {
                 Console.WriteLine("Installed updates require reboot.");
 
-                string pdqServer = Environment.GetEnvironmentVariable("PDQSERVER");
+                var pdqServer = Environment.GetEnvironmentVariable("PDQSERVER");
                 pdqServer = pdqServer?.Split('.')[0];
 
-                if (pdqServer != null
-                        && String.Compare(pdqServer, Environment.MachineName,
-                            StringComparison.InvariantCultureIgnoreCase) == 0) {
-                    File.Create(RebootFlagFileName);
-                } else {
+                if (pdqServer == null) {
+                    Console.WriteLine("Not running from PDQ Deploy; skipping required reboot.");
+                    return (int)Win32Error.ERROR_SUCCESS_REBOOT_REQUIRED;
+                }
+
+                if (string.Compare(pdqServer, Environment.MachineName,
+                    StringComparison.InvariantCultureIgnoreCase) == 0) {
                     Console.WriteLine("Skipping required reboot because this is " +
                                       "the PDQ Deploy Server.");
-                    return (int)BetterWin32Errors.Win32Error.ERROR_SUCCESS_REBOOT_REQUIRED;
+                    return (int)Win32Error.ERROR_SUCCESS_REBOOT_REQUIRED;
                 }
+
+                File.Create(RebootFlagFileName);
             }
 
-            return 0;
+            return (int)BetterWin32Errors.Win32Error.ERROR_SUCCESS;
         }
     }
 }
